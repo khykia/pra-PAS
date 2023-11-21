4ac55f89446fbef5170247d951cfa5c725a4d3fa
khykia <rizkiakhosyamalia8@gmail.com>
2023-11-21 08:04:46 +0700

    PAS 20-11-2023

--- resources/views/holytea/holytea.blade.php
+++ resources/views/holytea/holytea.blade.php
@@ -15,10 +15,10 @@
     <tbody class="table-group-divider">
       @foreach ($holytea as $key => $datas)
       <tr>
-        <td>{{ $key + 1 }}</td>
+        <td>{{ $key + 1}}</td>
         <td>{{ $datas->nama}}</td>
-        <td>{{ $datas->jenis }}</td>
-        <td>{{ $datas->harga }}</td>
+        <td>{{ $datas->jenis}}</td>
+        <td>{{ $datas->harga}}</td>
         <td>
           <a type="button" class="btn btn-primary" href="/holytea/detailholytea/{{$datas->id}}">Detail</a>
         </td>
